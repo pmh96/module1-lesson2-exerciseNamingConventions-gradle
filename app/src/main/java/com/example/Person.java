9e9811c67dc2f95eeace154d308689168231b766
Pascal Maximilian Haas <pascal.haas@compax.at>
2024-06-13 19:35:30 +0200

    solve it

--- app/src/main/java/com/example/Person.java
+++ app/src/main/java/com/example/Person.java
@@ -1,26 +1,26 @@
-package COM_EXAMPLE; // Package name
+package com.example; // Package name
 
 public class Person { // Class name: UpperCamelCase -> First Letter is Uppercase! -> here it is
                       // already correct, check the rest!
 
-    public static final int max-age=120; // Constant
+    public static final int MAX_AGE = 120; // Constant
 
-    public String First_Name; // Instance variable
-    public String Last_Name; // Instance variable
-    public int Age; // Instance variable
+    public String firstName; // Instance variable
+    public String lastName; // Instance variable
+    public int age; // Instance variable
 
-    public String GET_FULL_NAME() { // Method name
-        return First_Name + " " + Last_Name;
+    public String getFullName() { // Method name
+        return firstName + " " + lastName;
     }
 
-    public int GET_AGE_IN_MONTHS() { // Method name
-        return Age * 12;
+    public int getAgeInMonths() { // Method name
+        return age * 12;
     }
 
     public static void main(String[] args) { // Main method (also normal method name rule applies here)
-        Person Person1 = new Person(); // Local variable
+        Person person1 = new Person(); // Local variable
 
-        System.out.println("Person's full name: " + Person1.GET_FULL_NAME());
-        System.out.println("Age in months: " + Person1.GET_AGE_IN_MONTHS());
+        System.out.println("Person's full name: " + person1.getFullName());
+        System.out.println("Age in months: " + person1.getAgeInMonths());
     }
 }
